7d06fabae6d500043ab3ef98ccc103e3e324f063
gavinmorrow <gavinfmorrow@gmail.com>
2023-04-02 14:17:06 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::{get, post, delete}, Router};
+
+mod routes;
+mod model;
+
+const ROOT_PATH: &str = "127.0.0.1:7878";
+
+#[tokio::main]
+async fn main() {
+    println!("Starting golem server at {}", ROOT_PATH);
+
+    let app = Router::new();
+
+    axum::Server::bind(&ROOT_PATH.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
