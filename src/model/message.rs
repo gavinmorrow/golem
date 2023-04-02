772a8e2bb47eb1245ada7ca2eb694b7e6a9dd9f2
gavinmorrow <gavinfmorrow@gmail.com>
2023-04-02 14:17:00 -0400

    Update message.rs

--- src/model/message.rs
+++ src/model/message.rs
@@ -1,6 +1,10 @@
+use super::Snowflake;
+
+pub type Id = Snowflake;
+
 pub struct Message {
-	pub id: Snowflake,
-	pub author: User,
-	pub parent: Option<Snowflake>,
+	pub id: Id,
+	pub author: super::user::Id,
+	pub parent: Option<Id>,
 	pub content: String,
 }
