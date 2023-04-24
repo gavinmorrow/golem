5b8c509017d1ad22e2e71f92e6fa7496eb3ff43c
Gavin Morrow <gavinfmorrow@gmail.com>
2023-04-24 19:38:50 -0400

    Add an id field to `Session`

--- src/model/session.rs
+++ src/model/session.rs
@@ -1,16 +1,19 @@
 use super::Snowflake;
 
+pub type Id = Snowflake;
 pub type Token = u64;
 
 pub struct Session {
+    pub id: Id,
     pub token: Token,
     pub user_id: super::user::Id,
 }
 
 impl Session {
-    pub fn new(session_token: Token, user_id: super::user::Id) -> Session {
+    pub fn new(id: Id, token: Token, user_id: super::user::Id) -> Session {
         Session {
-            token: session_token,
+            id,
+            token,
             user_id,
         }
     }
