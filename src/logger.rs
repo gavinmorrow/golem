3a116a6d00b38b2ef4711b4271bc5c737156973f
Gavin Morrow <gavinfmorrow@gmail.com>
2023-04-27 18:58:11 -0400

    Convert logging to fern

--- src/logger.rs
+++ src/logger.rs
@@ -1,7 +1,34 @@
+use std::time::SystemTime;
+
+use fern::{
+    colors::{Color, ColoredLevelConfig},
+    Dispatch,
+};
 use log::LevelFilter;
-use simple_logger::SimpleLogger;
 
 pub fn init() {
-    SimpleLogger::new().init().unwrap();
-    log::set_max_level(LevelFilter::Trace);
+    let colors = ColoredLevelConfig::new()
+        .info(Color::Green)
+        .trace(Color::BrightBlack);
+
+    Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                // Color the info the level color, and the message bright white
+                "{}[{} {} {}]\x1B[0m {}{}\x1B[0m",
+                format_args!("\x1B[{}m", colors.get_color(&record.level()).to_fg_str()),
+                humantime::format_rfc3339_seconds(SystemTime::now()),
+                colors.color(record.level()),
+                record.target(),
+                format_args!("\x1B[{}m", Color::BrightWhite.to_fg_str()),
+                message
+            ))
+        })
+        // Only info from dependencies
+        .level(LevelFilter::Info)
+        // But everything for this crate (golem)
+        .level_for("golem", LevelFilter::Trace)
+        .chain(std::io::stdout())
+        .apply()
+        .expect("logger should initialize");
 }
