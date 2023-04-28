09ec6aa64ed3ffaac0012d5cb01191574c56a324
Gavin Morrow <gavinfmorrow@gmail.com>
2023-04-27 20:43:21 -0400

    Update ws.rs

--- src/routes/ws.rs
+++ src/routes/ws.rs
@@ -7,30 +7,35 @@ use axum::{
     Router,
 };
 use axum_macros::debug_handler;
+use log::{debug, trace};
 
 use crate::model::AppState;
 
 pub fn router() -> Router<Arc<AppState>> {
+    trace!("ws router");
     Router::<Arc<AppState>>::new().route("/", get(handler))
 }
 
 #[debug_handler]
 async fn handler(ws: WebSocketUpgrade, State(state): State<Arc<AppState>>) -> Response {
+    trace!("ws connection requested");
     ws.on_upgrade(|ws| handle_ws(ws, state))
 }
 
 async fn handle_ws(mut ws: WebSocket, state: Arc<AppState>) {
-	while let Some(msg) = ws.recv().await {
-        let msg = if let Ok(msg) = msg {
-            msg
-        } else {
+    trace!("ws connection opened");
+    while let Some(msg) = ws.recv().await {
+        let Ok(msg) = msg else {
             // client disconnected
-            return;
+            break;
         };
+
+        debug!("ws message: {:?}", msg);
 
         if ws.send(msg).await.is_err() {
             // client disconnected
-            return;
+            break;
         }
     }
+    trace!("ws connection closed");
 }
