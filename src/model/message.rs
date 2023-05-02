f6c16f7de49ab79679a8b118539a9fa76405165b
Gavin Morrow <gavinfmorrow@gmail.com>
2023-05-02 17:00:24 -0400

    Derive Clone for Message

--- src/model/message.rs
+++ src/model/message.rs
@@ -2,7 +2,7 @@ use super::{user, Snowflake};
 
 pub type Id = Snowflake;
 
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
 pub struct Message {
     pub id: Id,
     pub author: user::Id,
