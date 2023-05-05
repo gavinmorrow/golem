1c721640c981f1d024a9ba79eaa0dacc1ba7d8c9
Gavin Morrow <gavinfmorrow@gmail.com>
2023-05-04 20:08:35 -0400

    Extract inline closure to function

--- src/routes/ws.rs
+++ src/routes/ws.rs
@@ -74,39 +74,11 @@ async fn handle_ws(ws: WebSocket, state: Arc<AppState>, tx: Sender) {
     let id = state.next_snowflake().id();
 
     // Split ws to send and receive at the same time
-    let (mut sender, mut receiver) = ws.split();
-    let mut rx = tx.subscribe();
+    let (sender, mut receiver) = ws.split();
+    let rx = tx.subscribe();
 
     // Send messages
-    let mut send_task = tokio::spawn(async move {
-        while let Ok(msg) = rx.recv().await {
-            // Check the target
-            // If it's all or the current id, send it
-            // Otherwise ignore it
-            match msg.target {
-                broadcast_msg::Target::All => {
-                    debug!("sending message from broadcast: {:?}", msg.content)
-                }
-                broadcast_msg::Target::One(target_id) => {
-                    if id == target_id {
-                        // Yup! A special message just for us!
-                        debug!("sending message to ws {}: {:?}", id, msg.content);
-                    } else {
-                        // Not for us
-                        continue;
-                    }
-                }
-            }
-            if sender
-                .send(Into::<String>::into(msg.content).into())
-                .await
-                .is_err()
-            {
-                // client disconnected
-                break;
-            }
-        }
-    });
+    let mut send_task = tokio::spawn(broadcast_handler(rx, id, sender));
 
     let tx = tx.clone();
 
@@ -166,6 +138,40 @@ async fn handle_ws(ws: WebSocket, state: Arc<AppState>, tx: Sender) {
     };
 
     trace!("ws connection closed");
+}
+
+async fn broadcast_handler(
+    mut rx: broadcast::Receiver<Broadcast>,
+    id: i64,
+    mut sender: futures::stream::SplitSink<WebSocket, ws::Message>,
+) {
+    while let Ok(msg) = rx.recv().await {
+        // Check the target
+        // If it's all or the current id, send it
+        // Otherwise ignore it
+        match msg.target {
+            broadcast_msg::Target::All => {
+                debug!("sending message from broadcast: {:?}", msg.content)
+            }
+            broadcast_msg::Target::One(target_id) => {
+                if id == target_id {
+                    // Yup! A special message just for us!
+                    debug!("sending message to ws {}: {:?}", id, msg.content);
+                } else {
+                    // Not for us
+                    continue;
+                }
+            }
+        }
+        if sender
+            .send(Into::<String>::into(msg.content).into())
+            .await
+            .is_err()
+        {
+            // client disconnected
+            break;
+        }
+    }
 }
 
 async fn handle_message(
