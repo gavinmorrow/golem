1ebadae3b1fca4876462d34dc7908b49ac11aab8
Gavin Morrow <gavinfmorrow@gmail.com>
2023-05-16 06:40:15 -0400

    Fix some bugs and add more logs

--- src/routes/ws/recv/msg_handler.rs
+++ src/routes/ws/recv/msg_handler.rs
@@ -8,6 +8,7 @@ use crate::{auth, model::Message};
 use super::super::{AppState, ServerMsg, Session};
 use super::msg::{ClientMsg, PartialUser, SendMessage};
 
+#[derive(Debug)]
 pub(super) enum HandlerResult {
     Reply(ServerMsg),
     Broadcast(ServerMsg),
@@ -50,6 +51,8 @@ async fn authenticate_token(
     token: u64,
     session: &mut Option<Session>,
 ) -> Response {
+    trace!("Authenticating user from token");
+
     let database = state.database.lock().await;
     let Ok(session_db) = auth::verify_session(token, database) else {
         // Client sent invalid token
@@ -59,6 +62,9 @@ async fn authenticate_token(
     let user_id = session_db.user_id.clone();
     *session = Some(session_db);
 
+    // Don't need to drop database here because it was moved into the auth::verify_session function
+    // and is dropped there.
+
     finish_authentication(state, user_id).await
 }
 
@@ -67,11 +73,14 @@ async fn authenticate(
     user: PartialUser,
     session: &mut Option<Session>,
 ) -> Response {
+    trace!("Authenticating user from credentials");
+
     let database = state.database.lock().await;
     let user_db = match database.get_user_by_name(&user.name) {
         Ok(Some(user)) => user,
         Ok(None) => {
             // User doesn't exist
+            error!("User not found in database: {}", user.name);
             return vec![Reply(ServerMsg::Authenticate { success: false })];
         }
         Err(err) => {
@@ -88,12 +97,14 @@ async fn authenticate(
     let user_id = user_db.id.clone();
     *session = Some(Session::generate(state.next_snowflake(), user_db.id));
 
+    drop(database); // Prevent deadlock
+
     finish_authentication(state, user_id).await
 }
 
 async fn finish_authentication(state: &Arc<AppState>, user_id: crate::model::user::Id) -> Response {
     // resolve user
-    let user = match state.database.lock().await.get_user(&user_id) {
+    let mut user = match state.database.lock().await.get_user(&user_id) {
         Ok(Some(user)) => user,
         Ok(None) => {
             error!("User {:?} not found in database.", user_id);
@@ -104,6 +115,10 @@ async fn finish_authentication(state: &Arc<AppState>, user_id: crate::model::use
             return vec![Reply(ServerMsg::Error)];
         }
     };
+
+    debug!("Finished authentication for user {:?}", user_id);
+
+    user.password = String::new(); // Don't send password to client
 
     vec![
         Reply(ServerMsg::Authenticate { success: true }),
@@ -117,6 +132,8 @@ async fn message(
     dedup_ids: &mut Vec<Option<String>>,
     message: SendMessage,
 ) -> Response {
+    debug!("Received message from client {}", session.id);
+
     let id = state.next_snowflake();
 
     let dedup_id = message.dedup_id.clone();
@@ -150,6 +167,7 @@ async fn message(
 }
 
 async fn load_all_messages(state: &Arc<AppState>) -> Response {
+    trace!("Loading all messages");
     let database = state.database.lock().await;
     match database.get_messages() {
         Ok(messages) => vec![Reply(ServerMsg::Messages(messages))],
