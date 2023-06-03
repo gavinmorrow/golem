cccf8b158d5176a331d62ce31c45ffdd746d4ecb
Gavin Morrow <gavinfmorrow@gmail.com>
2023-06-03 17:35:15 -0400

    something

--- src/templates.rs
+++ src/templates.rs
@@ -1,30 +1,33 @@
-use axum::{response::Html, Router};
+use axum::{extract::State, response::Html, Router};
 use axum_macros::debug_handler;
 use tera::{Context, Tera};
 use tower_http::services::ServeDir;
 
-lazy_static::lazy_static! {
-    pub static ref TEMPLATES: Tera = {
-        let tera = match Tera::new("templates/**/*") {
-            Ok(t) => t,
-            Err(e) => {
-                panic!("Parsing error(s): {}", e);
-            }
-        };
-        tera
-    };
+#[derive(Clone, Debug)]
+struct TemplateState {
+    templates: Tera,
 }
 
 pub fn router() -> Router {
+    let templates = match Tera::new("templates/**/*") {
+        Ok(t) => t,
+        Err(e) => {
+            panic!("Parsing error(s): {}", e);
+        }
+    };
+
+    let state = TemplateState { templates };
+
     Router::new()
-        .route("/a", axum::routing::get(index))
-        .nest_service("/", ServeDir::new("public"))
+        .route("/", axum::routing::get(index))
+        .nest_service("/static", ServeDir::new("public"))
+        .with_state(state)
 }
 
 #[debug_handler]
-async fn index() -> Html<String> {
+async fn index(State(state): State<TemplateState>) -> Html<String> {
     let mut context = Context::new();
     context.insert("title", "Hello, world!");
-    let rendered = TEMPLATES.render("index.html", &context).unwrap();
+    let rendered = state.templates.render("index.html", &context).unwrap();
     Html(rendered)
 }
